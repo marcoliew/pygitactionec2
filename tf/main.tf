98d7e40b638d0ad70a93ffff2ec89b8aaf4d1557
Marco Liao <marco.liao@sydney.edu.au>
2024-04-28 13:23:24 +1000

    save change

--- tf/main.tf
+++ tf/main.tf
@@ -7,30 +7,30 @@ terraform {
   required_version = ">= 0.12.0"
 }
 
-data "aws_vpc" "stagingtest" {
-  id = var.vpc_id
-}
+# data "aws_vpc" "stagingtest" {
+#   id = var.vpc_id
+# }
 
-data "aws_subnets" "dest" {
-  filter {
-    name   = "vpc-id"
-    values = [data.aws_vpc.stagingtest.id]
-  }
-  filter {
-    name   = "tag:Name"
-    values = "public-ap-southeast-2b"
-  }  
+# data "aws_subnets" "dest" {
+#   filter {
+#     name   = "vpc-id"
+#     values = [data.aws_vpc.stagingtest.id]
+#   }
+#   filter {
+#     name   = "tag:Name"
+#     values = "public-ap-southeast-2b"
+#   }  
 
-}
+# }
 
-data "aws_subnet" "dest" {
-  for_each = toset(data.aws_subnets.dest.ids)
-  id       = each.value
-}
+# data "aws_subnet" "dest" {
+#   for_each = toset(data.aws_subnets.dest.ids)
+#   id       = each.value
+# }
 
-output "subnet_cidr_blocks" {
-  value = [for s in data.aws_subnet.dest : s.cidr_block]
-}
+# output "subnet_cidr_blocks" {
+#   value = [for s in data.aws_subnet.dest : s.cidr_block]
+# }
           
 resource "aws_ecr_repository" "cs" {
   name                 = "pygitactionec2"
@@ -64,12 +64,103 @@ output "latest" {
   value = data.aws_ami.ubuntu
 }
 
-resource "aws_instance" "ec2" {
-  ami           = data.aws_ami.ubuntu.image_id
-  instance_type = "t2.micro"
-  subnet_id = data.aws_subnet.dest.id
+# resource "aws_instance" "ec2" {
+#   ami           = data.aws_ami.ubuntu.image_id
+#   instance_type = "t2.micro"
+#   subnet_id = data.aws_subnet.dest.id
+
+#   tags = {
+#     Name = "pygitactionec2"
+#   }
+# }
+
+# instance identity
+resource "aws_instance" "pygitactionec2" {
+  ami                         = lookup(var.awsprops, "ami")
+  instance_type               = lookup(var.awsprops, "itype")
+  subnet_id                   = lookup(var.awsprops, "subnet")
+  associate_public_ip_address = lookup(var.awsprops, "publicip")
+  key_name                    = lookup(var.awsprops, "keyname")
+
+
+  # security group
+  vpc_security_group_ids = [
+    aws_security_group.pygitactionec2-sg.id
+  ]
+  root_block_device {
+    delete_on_termination = true
+    volume_size           = 40
+    volume_type           = "gp2"
+  }
+  tags = {
+    Name        = "pygitactionec2"
+    Environment = "DEV"
+    OS          = "UBUNTU"
+    Managed     = "TF"
+  }
+
+  provisioner "file" {
+    source      = "installer.sh"
+    destination = "/tmp/installer.sh"
+
+  }
+
+  provisioner "remote-exec" {
+    inline = [
+      "sudo chmod +x /tmp/installer.sh",
+      "sh /tmp/installer.sh"
+    ]
+
+  }
+  depends_on = [aws_security_group.pygitactionec2-sg]
+
+# connecting to AWS instance to install jenkins and docker
+  connection {
+    type        = "ssh"
+    host        = self.public_ip
+    user        = "ubuntu"
+    private_key = file("./pygitactionec2-key.pem")
+  }
+}
+
+
+output "ec2instance" {
+  value = aws_instance.pygitactionec2.public_ip
+}
+
+
+// AMI Security group setting using HashiCorp Configuration Language (HCL)
+resource "aws_security_group" "pygitactionec2-sg" {
+  name        = lookup(var.awsprops, "secgroupname")
+  description = lookup(var.awsprops, "secgroupname")
+  vpc_id      = lookup(var.awsprops, "vpc")
+
+  // To Allow SSH Transport
+
+  dynamic "ingress" {
+    for_each = var.ingress_rules
+    content {
+      description = lookup(ingress.value, "description", null)
+      from_port   = lookup(ingress.value, "from_port", null)
+      to_port     = lookup(ingress.value, "to_port", null)
+      protocol    = lookup(ingress.value, "protocol", null)
+      cidr_blocks = lookup(ingress.value, "cidr_blocks", null)
+    }
+  }
+
+  egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
 
   tags = {
-    Name = "pygitactionec2"
+    Name = "allow_tls"
+  }
+
+  lifecycle {
+    create_before_destroy = false
   }
 }
--- tf/variables.tf
+++ tf/variables.tf
@@ -1,6 +1,131 @@
 
-variable "vpc_id" {
-  description = "id of vpc"
-  type        = string
-  default      = "vpc-08112924827b29757"
+# variable "vpc_id" {
+#   description = "id of vpc"
+#   type        = string
+#   default      = "vpc-08112924827b29757"
+# }
+
+variable "awsprops" {
+  type = map(any)
+  default = {
+    region       = "ap-southeast-2"
+    vpc          = "vpc-08112924827b29757"
+    ami          = "ami-080660c9757080771"
+    itype        = "t2.micro"
+    subnet       = "subnet-0273c400b9d31a60f"
+    publicip     = true
+    keyname      = "pygitactionec2-key"
+    secgroupname = "pygitactionec2-sg"
+  }
 }
+
+variable "ingress_rules" {
+  default = {
+    "my ingress rule" = {
+      "description" = "For HTTP"
+      "from_port"   = "80"
+      "to_port"     = "80"
+      "protocol"    = "tcp"
+      "cidr_blocks" = ["0.0.0.0/0"]
+    },
+    "my other ingress rule" = {
+      "description" = "For SSH"
+      "from_port"   = "22"
+      "to_port"     = "22"
+      "protocol"    = "tcp"
+      "cidr_blocks" = ["0.0.0.0/0"]
+    },
+
+    "Postgres port" = {
+      "description" = "For HTTP postgres"
+      "from_port"   = "5432"
+      "to_port"     = "5432"
+      "protocol"    = "tcp"
+      "cidr_blocks" = ["0.0.0.0/0"]
+    },
+
+    "Jenkins port" = {
+      "description" = "For Jenkins"
+      "from_port"   = "8080"
+      "to_port"     = "8080"
+      "protocol"    = "tcp"
+      "cidr_blocks" = ["0.0.0.0/0"]
+    },
+
+     "React Application port" = {
+      "description" = "For React"
+      "from_port"   = "3000"
+      "to_port"     = "3000"
+      "protocol"    = "tcp"
+      "cidr_blocks" = ["0.0.0.0/0"]
+    },
+
+    "Django Application port" = {
+      "description" = "For Django"
+      "from_port"   = "8585"
+      "to_port"     = "8585"
+      "protocol"    = "tcp"
+      "cidr_blocks" = ["0.0.0.0/0"]
+    },
+     "Django alt Application port" = {
+      "description" = "For Django alt port"
+      "from_port"   = "8000"
+      "to_port"     = "8000"
+      "protocol"    = "tcp"
+      "cidr_blocks" = ["0.0.0.0/0"]
+    }
+
+    "All Ports" = {
+      "description" = "For HTTP all ports"
+      "from_port"   = "3000"
+      "to_port"     = "65535"
+      "protocol"    = "tcp"
+      "cidr_blocks" = ["0.0.0.0/0"]
+    }
+  }
+  type = map(object({
+    description = string
+    from_port   = number
+    to_port     = number
+    protocol    = string
+    cidr_blocks = list(string)
+  }))
+  description = "Security group rules"
+}
+
+# variable "instance_type" {
+#   type    = string
+#   default = "t2.micro"
+# }
+
+# variable "instance_ami" {
+#   description = "AMI ID for the EC2 instance"
+#   type        = string
+#   default     = ""
+# }
+
+# variable "instance_vpc_id" {
+#   type    = string
+#   default = ""
+# }
+
+# variable "instance_subnet_id" {
+#   type    = string
+#   default = ""
+# }
+
+# variable "instance_keyName" {
+#   type    = string
+#   default = "pygitactionec2-key"
+# }
+
+# variable "instance_secgroupname" {
+#   description = "This is a security Group Name"
+#   type        = string
+#   default     = "pygitactionec2-sg"
+# }
+
+# variable "instance_publicip" {
+#   type    = bool
+#   default = true
+# }
